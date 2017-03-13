6e63c2bbeec31c27b24c547685702ea7e1c5cfc0
Matt <mmcgeehan@paduaacademy.org>
2017-03-13 10:12:02 -0400

    Add files via upload

---
+++ Dictionaries.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var person = ["Taylor", "Cate", "Jim Bob Circle Pants","Lauren", "Shaquill O Neill"]
+
+
+person[0]
+
+
+person[3]
+
+
+
+
